34bc5bbd9983e1fb15e7a26ff62675a093371eac
Bill Randall <william.randall@cru.org>
2016-11-05 11:03:50 -0400

    Rename module

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('wbmLesson', ['ionic', 'wbmLesson.controllers', 'wbmLesson.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', [])
+angular.module('wbmLesson.controllers', [])
 
 .controller('watchController', function($scope) {})
 
