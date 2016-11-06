bb1769e3fd22ae8230f7061821a88da4b2298656
Bill Randall <william.randall@cru.org>
2016-11-05 21:38:00 -0400

    Remove unused files

--- www/js/app.js
+++ www/js/app.js
@@ -3,8 +3,6 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
 angular.module('wbmLesson', ['ionic', 'ngCordova', 'userMessages'])
 
 .run(function($ionicPlatform) {
@@ -28,7 +26,6 @@ angular.module('wbmLesson', ['ionic', 'ngCordova', 'userMessages'])
   // Ionic uses AngularUI Router which uses the concept of states
   // Learn more here: https://github.com/angular-ui/ui-router
   // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
   $stateProvider
 
   .state('UserMessages', {
