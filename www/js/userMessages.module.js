809c106dfe3d5632bfb14b3cdc197630b33c77cc
dsgoers <daniel.goers@cru.org>
2016-11-06 12:17:58 -0500

    Edit the mock conversation.

--- www/js/userMessages.module.js
+++ www/js/userMessages.module.js
@@ -10,7 +10,7 @@ angular.module('userMessages', [])
             $scope.toUser = {
                 _id: '534b8e5aaa5e7afc1b23e69b',
                 pic: 'http://ionicframework.com/img/docs/venkman.jpg',
-                username: 'Venkman'
+                username: 'Peter Venkman'
             }
 
             // this could be on $rootScope rather than in $stateParams
@@ -77,7 +77,7 @@ angular.module('userMessages', [])
                             "_id": "535d625f898df4e85e2a125d",
                             "text": questionText,
                             "userId": "534b8e5aaa5e7afc1b23e69b",
-                            "date": new Date(),
+                            "date": 'Just now',
                             "read": true,
                             "readDate": new Date()
                         };
@@ -111,7 +111,7 @@ angular.module('userMessages', [])
                 $scope.input.message = '';
 
                 message._id = new Date().getTime(); // :~)
-                message.date = new Date();
+                message.date = 'Just now';
                 message.username = $scope.user.username;
                 message.userId = $scope.user._id;
                 message.pic = $scope.user.picture;
@@ -229,7 +229,7 @@ angular.module('userMessages', [])
             me.getMockMessage = function() {
                 return {
                     userId: '534b8e5aaa5e7afc1b23e69b',
-                    date: new Date(),
+                    date: 'Just now',
                     text: 'Here\'s the links again.  You can watch, listen, or read the lesson.',
                     link: {
                         watch: '#/tab/watch',
@@ -310,68 +310,33 @@ function onProfilePicError(ele) {
 function getMockMessages() {
     return {
         "messages": [{
-            "_id": "535d625f898df4e80e2a125e",
-            "text": "Ionic has changed the game for hybrid app development.",
-            "userId": "534b8fb2aa5e7afc1b23e69c",
-            "date": "2014-04-27T20:02:39.082Z",
-            "read": true,
-            "readDate": "2014-12-01T06:27:37.944Z"
-        }, {
             "_id": "535f13ffee3b2a68112b9fc0",
-            "text": "I like Ionic better than ice cream!",
+            "text": "Hey this is Peter Venkman.",
             "userId": "534b8e5aaa5e7afc1b23e69b",
-            "date": "2014-04-29T02:52:47.706Z",
+            "date": "Yesterday",
             "read": true,
             "readDate": "2014-12-01T06:27:37.944Z"
         }, {
             "_id": "546a5843fd4c5d581efa263a",
-            "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            "text": "Thanks.  I really enjoyed talking with you today.",
             "userId": "534b8fb2aa5e7afc1b23e69c",
-            "date": "2014-11-17T20:19:15.289Z",
+            "date": "3 hours ago",
             "read": true,
             "readDate": "2014-12-01T06:27:38.328Z"
         }, {
             "_id": "54764399ab43d1d4113abfd1",
-            "text": "Am I dreaming?",
+            "text": "Can I send you something to help you in your new relationship with Jesus Christ?",
             "userId": "534b8e5aaa5e7afc1b23e69b",
-            "date": "2014-11-26T21:18:17.591Z",
+            "date": "2 hours ago",
             "read": true,
             "readDate": "2014-12-01T06:27:38.337Z"
         }, {
             "_id": "547643aeab43d1d4113abfd2",
-            "text": "Is this magic?",
+            "text": "Sure.",
             "userId": "534b8fb2aa5e7afc1b23e69c",
-            "date": "2014-11-26T21:18:38.549Z",
+            "date": "1 hour ago",
             "read": true,
             "readDate": "2014-12-01T06:27:38.338Z"
-        }, {
-            "_id": "547815dbab43d1d4113abfef",
-            "text": "Gee wiz, this is something special.",
-            "userId": "534b8e5aaa5e7afc1b23e69b",
-            "date": "2014-11-28T06:27:40.001Z",
-            "read": true,
-            "readDate": "2014-12-01T06:27:38.338Z"
-        }, {
-            "_id": "54781c69ab43d1d4113abff0",
-            "text": "I think I like Ionic more than I like ice cream!",
-            "userId": "534b8fb2aa5e7afc1b23e69c",
-            "date": "2014-11-28T06:55:37.350Z",
-            "read": true,
-            "readDate": "2014-12-01T06:27:38.338Z"
-        }, {
-            "_id": "54781ca4ab43d1d4113abff1",
-            "text": "Yea, it's pretty sweet",
-            "userId": "534b8e5aaa5e7afc1b23e69b",
-            "date": "2014-11-28T06:56:36.472Z",
-            "read": true,
-            "readDate": "2014-12-01T06:27:38.338Z"
-        }, {
-            "_id": "5478df86ab43d1d4113abff4",
-            "text": "Wow, this is really something huh?",
-            "userId": "534b8fb2aa5e7afc1b23e69c",
-            "date": "2014-11-28T20:48:06.572Z",
-            "read": true,
-            "readDate": "2014-12-01T06:27:38.339Z"
         }, {
             "_id": "54781ca4ab43d1d4113abff1",
             "text": 'Check out this lesson on treasuring Jesus Christ.  You can watch, listen, or read it. ',
@@ -381,7 +346,7 @@ function getMockMessages() {
                 "read": '#/tab/read',
             },
             "userId": "534b8e5aaa5e7afc1b23e69b",
-            "date": "2014-11-29T06:56:36.472Z",
+            "date": "1 hour ago",
             "read": true,
             "readDate": "2014-12-01T06:27:38.338Z"
         }],
