a64a55f6953b0291a93f345a2583eda01c129639
dsgoers <daniel.goers@cru.org>
2016-11-06 12:46:51 -0500

    Add mentor navigation tabs.

---
+++ www/js/favorites.module.js
@@ -0,0 +1,15 @@
+/**
+ * Created by dsgoers on 11/6/16.
+ */
+angular.module('favorites', [])
+    .controller('favoritesController', ['$scope', '$rootScope', '$state',
+        '$stateParams', 'MockService', '$ionicActionSheet',
+        '$ionicPopup', '$ionicScrollDelegate', '$timeout', '$interval',
+
+        function($scope, $rootScope, $state, $stateParams, MockService,
+                 $ionicActionSheet,
+                 $ionicPopup, $ionicScrollDelegate, $timeout, $interval) {
+
+        }
+
+    ]);
